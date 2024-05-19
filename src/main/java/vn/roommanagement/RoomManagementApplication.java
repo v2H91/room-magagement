cdd0e67026c98561fc8f35872f5243a20f84fdc6
tuanpc <tuanvt1306@gmail.com>
2024-05-19 22:36:17 +0700

    Test commit

--- src/main/java/vn/roommanagement/RoomManagementApplication.java
+++ src/main/java/vn/roommanagement/RoomManagementApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RoomManagementApplication {
-
+  //Test git commit
 	public static void main(String[] args) {
 		SpringApplication.run(RoomManagementApplication.class, args);
 	}
