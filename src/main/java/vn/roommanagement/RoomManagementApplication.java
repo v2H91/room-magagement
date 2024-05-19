1229da8b5e87ef29301ae583dd100ec56f5c9237
hungvu <hungvu19061991@gmail.com>
2024-05-19 09:49:49 +0700

    first init

---
+++ src/main/java/vn/roommanagement/RoomManagementApplication.java
@@ -0,0 +1,13 @@
+package vn.roommanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/roommanagement/RoomManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.roommanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
