842ba9f459ae33c9a897bc5ea748adad99d1ba2c
hungvu <hungvu19061991@gmail.com>
2024-05-21 23:03:33 +0700

    push origin

--- src/main/java/vn/roommanagement/api/common/ResponseApi.java
+++ src/main/java/vn/roommanagement/api/common/ResponseApi.java
@@ -8,12 +8,5 @@ public class ResponseApi extends ResponseEntity<ObjectResponse> {
         super(new ObjectResponse(HttpStatus.OK.value(), data, "Success", ""), HttpStatus.OK);
     }
 
-    public ResponseApi(int status, Object data, String messageId, Object errorMessage, int statusApi) {
-        super(new ObjectResponse(status, data, messageId, errorMessage), HttpStatus.OK);
-    }
-
-    public ResponseApi(int status, Object data, String messageId, Object errorMessage, String statusApi) {
-        super(new ObjectResponse(status, data, messageId, errorMessage), HttpStatus.OK);
-    }
 
 }
